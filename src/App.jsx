cb1885b2db5311ef3b60a68f323a6f107d35d7de
Lewan Boegner <lele.exl@gmail.com>
2023-10-21 17:45:33 +0200

    uncommented the commented code ^^

--- src/App.jsx
+++ src/App.jsx
@@ -23,30 +23,29 @@ function App() {
 
   return (
     <>
-      <h1>hello</h1>
-      {/* {ipAddress ? ( */}
-      {/* <>
-        <h2 id="root">Your IP address is {ipAddress}</h2>
-        <MapContainer
-          center={position}
-          zoom={13}
-          scrollWheelZoom={false}
-          style={{ height: "350px", width: "350px" }}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://%7Bs%7D.tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png"
-          />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-              <img src="pic_trulli.jpg" alt="Italian Trulli" />
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </> */}
-      {/* ) : (
+      {ipAddress ? (
+        <>
+          <h2 id="root">Your IP address is {ipAddress}</h2>
+          <MapContainer
+            center={position}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "350px", width: "350px" }}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://%7Bs%7D.tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+                <img src="pic_trulli.jpg" alt="Italian Trulli" />
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </>
+      ) : (
         <p>Loading IP address...</p>
-      )} */}
+      )}
     </>
   );
 }
